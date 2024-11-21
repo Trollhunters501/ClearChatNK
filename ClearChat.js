05848ceecb89414225580ec594097e28256fe721
Trollhunters501PC <cpcreador@gmail.com>
2024-11-20 22:16:31 -0600

    Actualizar a JSENK2 y cambiar config.yml por config.json

--- ClearChat.js
+++ ClearChat.js
@@ -1,29 +1,24 @@
-var ConfigClCh = {
-    //Do you want to activate the chat auto emptying? Only false or true
-    autoClear: true,
-    //Just modify the number! the time in ticks example: 1200 = 1 minute
-    timeInterval: 18000,
-    //The message you send when clearing the Chat!
-    Message: "Se ha Limpiado el Chat!/Chat has been cleaned up!"
-};
+let ConfigClCh = {};
 //don't change the rest!
-var pluginInfClCh = {
-    PluginName: "ClearChat",
-    Version: "1.0",
-    WebSite: "https://github.com/Trollhunters501/Trollhunters501",
-    author: "Creadores Program"
-};
-var pluginNameClCh = "[" + pluginInfClCh.PluginName + "] ";
-function onEnable(){
+let pluginNameClCh = "[ClearChatNK] ";
+function load(){
+    let subconf = manager.getFile("ClearChatNK", "config.json");
+    if(!subconf.exists()){
+        let escritor = new java.io.FileWriter(subconf);
+        escritor.write(JSON.stringify(require("./config.json")));
+        escritor.close();
+    }
+    ConfigClCh = JSON.parse(readFully(subconf.getAbsolutePath()));
+}
+function enable(){
     logger.info(pluginNameClCh + "§eLoading...");
     if(ConfigClCh.autoClear != false){
-      manager.createLoopTask("clearChatF", ConfigClCh.timeInterval);
+      manager.createLoopTask(clearChatF, ConfigClCh.timeInterval);
     }
-    manager.createCommand("clearchat", "Vaciar tu chat!/Empty your chat!", "clearChatCommand");
+    manager.createCommand("clearchat", "Vaciar tu chat!/Empty your chat!", clearChatCommand);
     logger.info(pluginNameClCh + "§aLoaded!");
     logger.info(pluginNameClCh + "§1Powered By §aCreadores §1Program");
 }
-onEnable();
 function clearChatF(currentTick){
     server.broadcastMessage("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     server.broadcastMessage("§a" + pluginNameClCh + ConfigClCh.Message);
@@ -32,3 +27,7 @@ function clearChatCommand(sender, args){
     sender.sendMessage("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     sender.sendMessage("§a" + pluginNameClCh + ConfigClCh.Message);
 }
+module.exports = {
+    onEnable: enable,
+    onLoad: load
+};
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ClearChatNK
 Requirements:
-[Have the mod loader plugin](https://cloudburstmc.org/resources/modloader.108/)
+[JSEngineNK](https://cloudburstmc.org/resources/jsenginenk.939/)
+[JSENK2](https://cloudburstmc.org/resources/jsenk2.1017/)
 
 Description:
 A simple plugin to Empty Player Chat!
@@ -8,25 +9,25 @@ It also has Automatic Mode!
 The player can also clear their chat with the /clearchat command.
 
 Plugin Installation:
-Place the zip file in the ModLoader folder and unzip and delete the zip file
+Place the zip file in the JSENK folder and unzip and delete the zip file
 
 Setting:
-Edit the .js file that came out after unzipping the zip file
-```js
+Edit the .json file in JSEngineNK/ClearChatNK
+```json
 //On the first line you will find this:
-var ConfigClCh = {
+{
 
     //Do you want to activate the chat auto emptying? Only false or true
 
-    autoClear: true,
+    "autoClear": true,
 
     //Just modify the number! the time in seconds
 
-    timeInterval: 1700,
+    "timeInterval": 1700,
 
     //The message you send when clearing the Chat!
 
-    Message: "Se ha Limpiado el Chat!/Chat has been cleaned up!"
+    "Message": "Se ha Limpiado el Chat!/Chat has been cleaned up!"
 
 };
 ```
@@ -34,4 +35,4 @@ If you want to make plugins for your server, do not hesitate to Contact Us!
 Discord: https://discord.gg/mrmHcwxXff
 ![image](https://user-images.githubusercontent.com/91704943/230763339-a5b59ec0-8c59-4c98-8938-57f0ad2cb512.png)
 
-2023©
+2024©
